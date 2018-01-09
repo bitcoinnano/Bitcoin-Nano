5c4ebd665ab1617481b1d7a6c2d37ccef38b5861
bitcoinnano <bitcoinnano1@outlook.com>
2018-01-09 11:26:18 +0800

    modify copyright statements

--- src/pow/nano.h
+++ src/pow/nano.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2017-2018 The Bitcoin Nano developers                                                                                                                                                                                                                        
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef BTCNANO_NANO_H
 #define BTCNANO_NANO_H
 
